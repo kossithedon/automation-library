02e3828136ac7e501e9f27c2b683e0555eadb2b8
Kossi-junior AGBODJA <kjagbodja@hexanet.fr>
2025-11-07 16:02:55 +0100

    Cleaned remaining elements that already exists on the sekoïa console

--- Fortigate/main.py
+++ Fortigate/main.py
@@ -1,17 +1,11 @@
 # third parties
 from sekoia_automation.module import Module
 
-from fortigate.action_fortigate_add_fqdn import FortigateAddFQDNAction
-from fortigate.action_fortigate_add_group_address import FortigateAddGroupAddress
-
 # internals
-from fortigate.action_fortigate_add_ip_address import FortigateAddIPAction
 from fortigate.action_fortigate_disable_local_user import FortigateDisableLocalUserAction
 
 if __name__ == "__main__":
     module = Module()
-    module.register(FortigateAddIPAction, "fortigate_add_ip_address")
-    module.register(FortigateAddFQDNAction, "fortigate_add_fqdn")
-    module.register(FortigateAddGroupAddress, "fortigate_add_group_address")
+
     module.register(FortigateDisableLocalUserAction, "fortigate_disable_local_user")
     module.run()
